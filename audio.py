1ec1e924a4f9b65b4156d41f48bb9efa9ecc02f8
IlyaFurgal <fura1509@gmail.com>
2024-11-20 16:55:50 +0300

    test2

--- audio.py
+++ audio.py
@@ -5,13 +5,8 @@ from pytube import Search
 def download_audio(track_name):
     search = Search(track_name)
     result = search.results[0] if search.results else None
-
-    if not result:
-        print("Ничего не найдено. Попробуйте другое название.")
-        return
-
     video_url = result.watch_url
-    print(f"Скачиваю аудио с: {video_url}")
+
 
     # Настройки загрузки
     ydl_opts = {
@@ -29,4 +24,3 @@ def download_audio(track_name):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
 
-    print(f"Аудио '{track_name}' успешно загружено!")
